f9d15e06fe9546a9ca569ed8c9a1d11f42d7da86
korjyk <rustkayros@gmail.com>
2023-10-06 13:27:57 +0300

    ?

--- src/views/Deceased.rs
+++ src/views/Deceased.rs
@@ -38,11 +38,52 @@ use crate::schema;
 use crate::utils::establish_connection;
 
 
+
+//-------------------------------------------------------------------------
+
+
 pub fn deceased_routes(config: &mut web::ServiceConfig) {
     config.route("/deceased/{id}/", web::get().to(deceased_page));
+    config.route("/edit_deceased/{id}/", web::get().to(edit_deceased_page));
+    config.route("/created_deceased/", web::get().to(created_deceased_page));
     config.route("/all_deceased_place{id}/", web::get().to(all_deceased_place_page));
 }
 
+//-------------------------------------------------------------------------
 
 
 
+
+async fn get_request_user_id(req: &HttpRequest) -> Option<i32> { 
+    match Authorization::<Bearer>::parse(req) {
+        Ok(ok) => {
+            let token = ok.as_ref().token().to_string();
+            return match verify_jwt(token, "MYSECRETKEY").await {
+                Ok(ok) => ok.id,
+                Err(_) => None,
+            }
+        },
+        Err(_) => return None,
+    }
+}
+
+fn get_user(pk: i32) -> User {
+    use crate::schema::users::dsl::users;
+    let _connection = establish_connection();
+    return users
+        .filter(schema::users::id.eq(pk))
+        .first::<User>(&_connection)
+        .expect("E");
+}
+
+fn get_content_type<'a>(req: &'a HttpRequest) -> Option<&'a str> {
+    return req.headers().get("user-agent")?.to_str().ok();
+}
+pub fn is_desctop(req: &HttpRequest) -> bool {
+    if get_content_type(req).unwrap().contains("Mobile") {
+        return false;
+    };
+    return true;
+} 
+
+//-------------------------------------------------------------------------
