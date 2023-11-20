78192b07f8ed2d81dbbf71330258505d4e2eb26a
interesnij <ochkarik1983@mail.com>
2023-11-20 10:36:00 +0300

    H

--- src/schema.rs
+++ src/schema.rs
@@ -3,9 +3,7 @@
 diesel::table! {
     cities (id) {
         id -> Int4,
-        #[max_length = 100]
         name -> Varchar,
-        #[max_length = 100]
         name_ru -> Nullable<Varchar>,
         geo_id -> Nullable<Int4>,
         region_id -> Nullable<Int4>,
@@ -18,14 +16,11 @@ diesel::table! {
 diesel::table! {
     countries (id) {
         id -> Int4,
-        #[max_length = 100]
         name -> Varchar,
-        #[max_length = 100]
         name_ru -> Nullable<Varchar>,
         geo_id -> Nullable<Int4>,
         continent_id -> Nullable<Int4>,
         timezone_id -> Nullable<Int4>,
-        #[max_length = 10]
         phone -> Nullable<Varchar>,
         lat -> Nullable<Float8>,
         lon -> Nullable<Float8>,
@@ -37,17 +32,12 @@ diesel::table! {
         id -> Int4,
         user_id -> Int4,
         place_id -> Int4,
-        #[max_length = 100]
         first_name -> Varchar,
-        #[max_length = 100]
         middle_name -> Nullable<Varchar>,
-        #[max_length = 100]
         last_name -> Varchar,
         birth_date -> Date,
         death_date -> Date,
-        #[max_length = 100]
         image -> Nullable<Varchar>,
-        #[max_length = 500]
         memory_words -> Nullable<Varchar>,
         lat -> Float8,
         lon -> Float8,
@@ -57,19 +47,12 @@ diesel::table! {
 diesel::table! {
     organizations (id) {
         id -> Int4,
-        #[max_length = 100]
         name -> Varchar,
-        #[max_length = 500]
         description -> Varchar,
-        #[max_length = 255]
         director -> Varchar,
-        #[max_length = 15]
         phone -> Varchar,
-        #[max_length = 100]
         hours -> Varchar,
-        #[max_length = 100]
         website -> Nullable<Varchar>,
-        #[max_length = 100]
         image -> Nullable<Varchar>,
         user_id -> Int4,
     }
@@ -94,20 +77,13 @@ diesel::table! {
         city_id -> Int4,
         region_id -> Nullable<Int4>,
         country_id -> Int4,
-        #[max_length = 100]
         title -> Varchar,
-        #[max_length = 500]
         description -> Nullable<Varchar>,
-        #[max_length = 100]
         hours -> Nullable<Varchar>,
-        #[max_length = 100]
         image -> Nullable<Varchar>,
-        #[max_length = 255]
         address -> Nullable<Varchar>,
         count -> Int2,
-        #[max_length = 255]
         director -> Nullable<Varchar>,
-        #[max_length = 15]
         phone -> Nullable<Varchar>,
         lat -> Float8,
         lon -> Float8,
@@ -117,9 +93,7 @@ diesel::table! {
 diesel::table! {
     regions (id) {
         id -> Int4,
-        #[max_length = 100]
         name -> Varchar,
-        #[max_length = 100]
         name_ru -> Nullable<Varchar>,
         geo_id -> Nullable<Int4>,
         country_id -> Int4,
@@ -134,7 +108,6 @@ diesel::table! {
         id -> Int4,
         service_id -> Int4,
         user_id -> Int4,
-        #[max_length = 1000]
         content -> Varchar,
         created -> Timestamp,
     }
@@ -146,11 +119,8 @@ diesel::table! {
         user_id -> Int4,
         organization_id -> Int4,
         city_id -> Int4,
-        #[max_length = 100]
         title -> Varchar,
-        #[max_length = 1000]
         description -> Varchar,
-        #[max_length = 100]
         image -> Nullable<Varchar>,
         price -> Int4,
     }
@@ -159,14 +129,10 @@ diesel::table! {
 diesel::table! {
     users (id) {
         id -> Int4,
-        #[max_length = 100]
         username -> Varchar,
-        #[max_length = 100]
         email -> Varchar,
-        #[max_length = 100]
         password -> Varchar,
         description -> Nullable<Text>,
-        #[max_length = 100]
         image -> Nullable<Varchar>,
         perm -> Int2,
     }
