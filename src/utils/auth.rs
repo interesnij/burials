380556e067be91a9c065fe9b67b740b0fd3a989b
interesnij <ochkarik1983@mail.com>
2023-11-24 12:39:35 +0300

    J

--- src/utils/auth.rs
+++ src/utils/auth.rs
@@ -15,13 +15,14 @@ pub fn is_authenticate(token: &String)-> bool {
 }  
 
 pub fn set_token(token: &String, id: &String) {
-    let _local_token = web_local_storage_api::set_item(token, id);
-    let t = token.to_string();
-    let bearer = Bearer::new(t.clone());
-    let ne = &("Bearer".to_string() + &token.to_string());
-    let result = bearer.try_into_value();
-    assert!(result.is_ok());
+    //let _local_token = web_local_storage_api::set_item(token, id);
+    //let t = token.to_string();
+    //let bearer = Bearer::new(t.clone());
+    //let ne = &("Bearer".to_string() + &token.to_string());
+    //let result = bearer.try_into_value();
+    //assert!(result.is_ok());
     //Bearer::new(t);
+
 }
 
 pub async fn remove_token(req: &HttpRequest) -> i16 { 
@@ -37,19 +38,34 @@ pub async fn remove_token(req: &HttpRequest) -> i16 {
   }
 
 pub async fn get_request_user(req: &HttpRequest) -> Option<User> {
+  let _cookie_res = req.headers().get("cookie");
 
-  match Authorization::<Bearer>::parse(req) {
-    Ok(ok) => {
-      let token = ok.as_ref().token().to_string();
-      println!("token {:?}", token.clone());
-      return match crate::utils::verify_jwt(token).await {
-        Ok(ok) => {
-          println!("id {:?}", ok.id);
-          Some(crate::utils::get_user(ok.id).expect("E."))
-        },
-        Err(_) => None,
-      }
-    },
-    Err(_) => None,
-  }
+    if _cookie_res.is_none() {
+        return None;
+    }
+    let _cookie = _cookie_res.expect("E.").to_str().ok();
+    if _cookie.is_some() {
+        for c in _cookie.unwrap().split("; ").collect::<Vec<&str>>().iter() {
+            let split_c: Vec<&str> = c.split("=").collect();
+            if split_c[0] == "user" {
+                user_id = split_c[1].parse().unwrap();
+                Some(crate::utils::get_user(user_id).expect("E."))
+            }
+        }
+    }
+    return None;
+  //match Authorization::<Bearer>::parse(req) {
+  //  Ok(ok) => {
+  //    let token = ok.as_ref().token().to_string();
+  //    println!("token {:?}", token.clone());
+  //    return match crate::utils::verify_jwt(token).await {
+  //      Ok(ok) => {
+  //        println!("id {:?}", ok.id);
+  //        Some(crate::utils::get_user(ok.id).expect("E."))
+  //      },
+  //      Err(_) => None,
+  //    }
+  //  },
+  //  Err(_) => None,
+  //}
 }
--- src/views/auth.rs
+++ src/views/auth.rs
@@ -145,7 +145,7 @@ async fn handle_sign_in(data: LoginUser2, req: &HttpRequest) -> i16 {
                 let token = gen_jwt(_user.id).await;
                 match token {
                     Ok(token_str) => {
-                        crate::utils::set_token(&token_str, &_user.id.to_string());
+                        //crate::utils::set_token(&token_str, &_user.id.to_string());
                         return 1;
                     },
                     Err(err) => return 0,
