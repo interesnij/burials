781a6f436b1b205060e2c0ca127cbf6577a20a07
interesnij <ochkarik1983@mail.com>
2023-11-24 12:27:02 +0300

    J

--- src/utils/auth.rs
+++ src/utils/auth.rs
@@ -18,12 +18,12 @@ pub fn set_token(token: &String, id: &String) {
     let _local_token = web_local_storage_api::set_item(token, id);
     let t = token.to_string();
     let bearer = Bearer::new(t.clone());
-    let ne = "Bearer".to_string() + &t;
+    let ne = ("Bearer".to_string() + &token.to_string());
     let result = bearer.try_into_value();
     assert!(result.is_ok());
         assert_eq!(
             result.unwrap(),
-            HeaderValue::from_static(ne.as_str())
+            HeaderValue::from_static(ne)
         );
     //Bearer::new(t);
 }
