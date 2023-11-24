9b861af80a2ef352e1eee9423343eb35d7bcc629
interesnij <ochkarik1983@mail.com>
2023-11-24 12:18:41 +0300

    J

--- src/utils/auth.rs
+++ src/utils/auth.rs
@@ -16,14 +16,14 @@ pub fn is_authenticate(token: &String)-> bool {
 
 pub fn set_token(token: &String, id: &String) {
     let _local_token = web_local_storage_api::set_item(token, id);
-    let t = token.as_str();
+    let bearer = Bearer::new(token.as_str());
     let result = bearer.try_into_value();
     assert!(result.is_ok());
         assert_eq!(
             result.unwrap(),
             HeaderValue::from_static("Bearer mF_9.B5f-4.1JqM")
         );
-    Bearer::new(t);
+    //Bearer::new(t);
 }
 
 pub async fn remove_token(req: &HttpRequest) -> i16 { 
