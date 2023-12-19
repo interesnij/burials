8c3778ad1c04e3692c07208ac172d4c3306c65cb
interesnij <ochkarik1983@mail.com>
2023-12-19 22:19:10 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -253,7 +253,7 @@ on('body', 'click', '.search_deceaseds', function() {
     console.log("no death_date");
     death_date = today;
   }
-  console.log("_location len", len(_location));
+  console.log("_location len", _location.length);
 
   if (!_location) {
     console.log("no location");
