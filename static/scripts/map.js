71d8b51e40267c5950cb90c7511b45c00cc08ece
interesnij <ochkarik1983@mail.com>
2024-03-27 18:50:06 +0300

    J

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -238,7 +238,7 @@ function load_places_map(cord) {
     v.push(list[0]);
     v.push(list[1]);
     function init() {
-      console.log(cord);
+      console.log(cord_str);
       console.log(v);
       var suggestView = new ymaps.SuggestView('suggest1'),
         suggestView = new ymaps.SuggestView('suggest2'),
