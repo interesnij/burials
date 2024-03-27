3224373cac43e9f1f568cb181d684e3a47366e6e
interesnij <ochkarik1983@mail.com>
2024-03-27 10:21:55 +0300

    J

--- static/scripts/main6.js
+++ static/scripts/main6.js
@@ -1322,14 +1322,14 @@ function load_places_map(cord) {
   
     var map; 
     map = new ymaps.Map('map', {
-      center: [cord],
-      zoom: 14,
+      center: ["62.355295, 50.081872"],
+      zoom: 14, 
       controls: ["typeSelector", "fullscreenControl", "zoomControl", "geolocationControl"]
     });
   
     map.setType('yandex#satellite');
         map.geoObjects
-            .add(new ymaps.Placemark([load_places_map], {
+            .add(new ymaps.Placemark(["62.355295, 50.081872"], {
             balloonContent: '',
             iconCaption: ''
         }, {
